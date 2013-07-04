88a15ed560c0101c886f796460796084ab123a1f
Chuck Smith <pinchaque@gmail.com>
2013-07-04 15:27:39 -0700

    Get seeding working via a generalized class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,33 @@
 # Creates static or controlled value tables in the DB
 
-###########################################################################
-# CLEAR TABLES
-###########################################################################
-[
-  ActivityType,
-].each do |o|
-  puts("Clearing #{o}")
-  o.delete_all
+class FixedValueSeed
+  def initialize(klass, fstream = STDERR)
+    @klass = klass
+    @class_name = klass.name
+    @class_label = klass.name.underscore.to_sym
+    @fstream = fstream
+  end
+
+  def clear
+    @fstream.puts("Seeding #{@class_name}")
+    @klass.delete_all
+  end
+
+  def add_values(map)
+    map.each do |k, v|
+      @fstream.puts("  #{k} => #{v}")
+      FactoryGirl.create(@class_label, :id => k, :name => v)
+    end
+  end
+
+  def seed(map)
+    clear
+    add_values(map)
+  end
 end
 
-###########################################################################
-# FIXED VALUE TABLES
-###########################################################################
-puts('Filling ActivityType');
-[
-  { :id => ActivityType::RUN, :name => 'Run'},
-  { :id => ActivityType::CYCLE, :name => 'Cycle'},
-].each do |d|
-  puts("  #{d[:id]} => #{d[:name]}")
-  FactoryGirl.create(:activity_type, :id => d[:id], :name => d[:name])
-end
+FixedValueSeed.new(ActivityType).seed(
+  {
+    ActivityType::RUN => 'Run',
+    ActivityType::CYCLE => 'Cycle',
+  });
