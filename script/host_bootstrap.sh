05503078fcb24df7f5ed0d583e537d4e4153dab8
Chuck Smith <chuck@pinchaque.com>
2014-01-02 01:58:11 +0000

    Initial list of packages and gems needed

---
+++ script/host_bootstrap.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+sudo apt-get install \
+  g++ \
+  gcc \
+  autoconf \
+  automake \
+  binutils \
+  cpp \
+  dos2unix \
+  gdb \
+  make \
+  memcached \
+  nodejs \
+  postgresql-client-common \
+  postgresql-common \
+  postgresql \
+  ruby1.9.1-dev \
+  ruby1.9.1-examples \
+  ruby1.9.1-full \
+  ruby1.9.1 \
+  tree \
+  unzip \
+  zip \
+
+sudo gem install \
+  rails \
+  bcrypt-ruby \
+  factory_girl \
+  factory_girl_rails \
+  fastercsv \
+  forgery \
+  geocoder \
+  haml \
+  haml-rails \
+  meta_search \
+  nokogiri \
+  pg \
+  rubyzip \
+  seed-fu \
