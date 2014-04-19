0c6876ec71d437048e1eb2a26102955de1b70f96
Chuck Smith <chuck@pinchaque.com>
2014-04-19 20:59:00 +0000

    Change distance formula to miles and get working w/ unit tests

--- lib/util/geo.rb
+++ lib/util/geo.rb
@@ -1,23 +1,24 @@
 # Performing geographic manipulations and computations
-module Geo
-  # returns distance between two points on earth using haversine formula
+module Util::Geo
+  # returns distance in miles between two points on earth 
+  # using haversine formula
   # reference: http://www.movable-type.co.uk/scripts/latlong.html
   def distance(lat1, long1, lat2, long2)  
-    r_earth = 6378.14 
+    r_earth = 3963.19 # miles
     
     # convert to radians
-    rlat1, rlong1, rlat2, rlong2 = [lat1, long1, lat2, long2].map begin |d| 
+    rlat1, rlong1, rlat2, rlong2 = [lat1, long1, lat2, long2].map { |d| 
       d * Math::PI/180
-    end
+    }
 
     # deltas
-    dlon = rlong1 - rlong2
-    dlat = rlat1 - rlat2
+    dlat = rlat2 - rlat1
+    dlon = rlong2 - rlong1
 
-    a = (Math::sin(dlat/2) ** 2) 
-      + Math::cos(rlat1) * Math::cos(rlat2) * (Math::sin(dlon/2) ** 2)
+    a = (Math::sin(dlat/2.0)**2) + (Math::cos(rlat1) *  Math::cos(rlat2) * Math::sin(dlon/2.0)**2)
+    c = 2 * Math::asin(Math::sqrt(a))
 
-    r_earth * 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+    r_earth * c
   end
   
   module_function :distance
--- spec/lib/util/geo_spec.rb
+++ spec/lib/util/geo_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
-describe Geo do
+describe Util::Geo do
   it "distance" do
-    d = distance(36.12, -86.67, 33.94, -118.4)
-    expect(d).to be_within(0.0000001).of(2886.4)
+    d = Util::Geo::distance(36.12, -86.67, 33.94, -118.4)
+    expect(d).to be_within(0.1).of(1795.5624)
+
+    d = Util::Geo::distance(36.12, 40.0, 33.94, 44.0)
+    expect(d).to be_within(0.1).of(272.115)
   end
 end
